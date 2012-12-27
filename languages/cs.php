f0f2d3f56f44d22d2b5d811de6687102f328cd8a
cmb69 <cmbecker69@gmx.de>
2012-12-27 16:02:22 +0000

    added Czech localization, user manual and README file (contributed by oldnema)

---
+++ languages/cs.php
@@ -0,0 +1,16 @@
+<?php
+
+	$plugin_tx['slideshow']['syscheck_title']="Kontrola systému";
+	$plugin_tx['slideshow']['syscheck_phpversion']="PHP verze ≥ %s";
+	$plugin_tx['slideshow']['syscheck_extension']="Rozšíření '%s' načteno";
+	$plugin_tx['slideshow']['syscheck_encoding']="Kodování 'UTF-8' nastaveno";
+	$plugin_tx['slideshow']['syscheck_magic_quotes']="Magic quotes runtime off";
+	$plugin_tx['slideshow']['syscheck_writable']="Adresář '%s' je zapisovatelný";
+	$plugin_tx['slideshow']['cf_default_order']="Pořadí obrázků: \"fixed\" (abecedně seřazené, od prvního snímku), \"sorted\" (abecedně seřazeny; začít u náhodně vybraného snímku) nebo \"random\" (náhodně vybranét).";
+	$plugin_tx['slideshow']['cf_default_effect']="Druh přechodu: \"fade\", \"slide\", \"curtain\" nebo \"random\".";
+	$plugin_tx['slideshow']['cf_default_easing']="Efekt posunu: \"linear\", \"easeIn\", \"easeOut\" nebo \"easeInOut\".";
+	$plugin_tx['slideshow']['cf_default_delay']="Počáteční zpoždění v ms kdy se spustí prezentace.";
+	$plugin_tx['slideshow']['cf_default_pause']="Doba trvání pauzy mezi přechody v ms.";
+	$plugin_tx['slideshow']['cf_default_duration']="Doba trvání přechodového efektu v ms.";
+
+?>
