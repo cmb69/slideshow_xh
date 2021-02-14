e670a90831ffa49cfa246c6327b338b5cc57aa3e
Christoph M. Becker <cmbecker69@gmx.de>
2021-02-14 17:16:22 +0100

    Switch to GNU style file headers

--- slideshow.js
+++ slideshow.js
@@ -1,12 +1,21 @@
-/*!
- * JavaScript of Slideshow_XH.
+/**
+ * Copyright 2012-2015 Christoph M. Becker
  *
- * @copyright Copyright (c) 2012-2015 Christoph M. Becker <http://3-magi.net/>
- * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3
- * @link      <http://3-magi.net/?CMSimple_XH/Slideshow_XH>
- * @see       <https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/>
+ * This file is part of Slideshow_XH.
+ *
+ * Slideshow_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Slideshow_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Slideshow_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 /**
  * The plugin's namespace.
