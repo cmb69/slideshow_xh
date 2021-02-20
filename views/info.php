07bbcf89ae8d6538b1a01002ef7d44cfb546f840
Christoph M. Becker <cmbecker69@gmx.de>
2021-02-20 16:23:14 +0100

    Use short echo tags
    We also fix the erroneous PHP snippet in the plugin synopsis.

--- views/info.php
+++ views/info.php
@@ -8,10 +8,8 @@ if (!isset($this)) {
 <!-- Slideshow_XH info -->
 <h1>Slideshow <?=$version?></h1>
 <h4><?=$tx['synopsis']?></h4>
-<pre>&lt;=slideshow('<?=$tx['synopsis_folder']?>', '<?php echo
-$tx['synopsis_options']?>');&gt;
-{{{slideshow('<?=$tx['synopsis_folder']?>', '<?php echo
-$tx['synopsis_options']?>');}}}</pre>
+<pre>&lt;?=slideshow('<?=$tx['synopsis_folder']?>', '<?=$tx['synopsis_options']?>')?&gt;
+{{{slideshow('<?=$tx['synopsis_folder']?>', '<?=$tx['synopsis_options']?>')}}}</pre>
 <dl>
     <dt><?=$tx['synopsis_folder']?>:</dt>
     <dd><?=$tx['synopsis_folder_desc']?></dd>
@@ -36,8 +34,8 @@ $tx['synopsis_options']?>');}}}</pre>
 <ul style="list-style: none">
 <?php foreach ($checks as $check => $state):?>
     <li>
-        <img src="<?=$images[$state]?>" alt="<?php echo
-        $images[$state]?>" style="margin: 0; height: 1em; padding-right: 1em">
+        <img src="<?=$images[$state]?>" alt="<?=$images[$state]?>"
+                style="margin: 0; height: 1em; padding-right: 1em">
         <span><?=$check?></span>
     </li>
 <?php endforeach?>
