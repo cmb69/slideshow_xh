c392560f5299711fef4f8625ebea1db31ee54b5e
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-19 21:19:33 +0100

    Clean up MainCommand to slideshow template data passing
    We should keep view templates as simple as possible, even at the cost
    of more complexity in the controllers.

--- classes/MainCommand.php
+++ classes/MainCommand.php
@@ -24,6 +24,7 @@ namespace Slideshow;
 use Plib\Request;
 use Plib\Response;
 use Plib\View;
+use Slideshow\Model\Image;
 use Slideshow\Model\ImageRepo;
 
 class MainCommand
@@ -60,32 +61,27 @@ class MainCommand
 
     public function __invoke(Request $request, string $path, string $options = ''): Response
     {
-        $opts = $this->getOptions($options);
+        $options = $this->getOptions($options);
         $path = $this->imageFolder . rtrim($path, '/') . '/';
-        $imgs = $this->imageRepo->findAll($path, $opts['order']);
+        $imgs = $this->imageRepo->findAll($path, $options["order"]);
         if (count($imgs) < 2) {
             if ($request->admin()) {
                 return Response::create($this->view->message("fail", "message_insufficient_images", $path));
             }
             return Response::create();
         }
-        $styles = $loading = [];
-        foreach ($imgs as $i => $img) {
-            $styles[] = $i === 0
-                ? "position: static; display: block; z-index: 1; width: 100%"
-                : "position: absolute; display: none; width: 100%";
-            $loading[] = $i === 0 ? "eager" : "lazy";
-        }
         return Response::create($this->view->render('slideshow', [
-            'imgs' => $imgs,
-            'styles' => $styles,
-            'loading' => $loading,
-            'opts' => $opts,
+            "images" => $this->imageData($imgs),
+            "effect" => $options["effect"],
+            "easing" => $options["easing"],
+            "delay" => $options["delay"],
+            "pause" => $options["pause"],
+            "duration" => $options["duration"],
             "script" => $this->pluginFolder . "slideshow.min.js",
         ]));
     }
 
-    /** @return array<string,string> */
+    /** @return array<'delay'|'duration'|'easing'|'effect'|'order'|'pause', string> */
     private function getOptions(string $query): array
     {
         $validOpts = ['order', 'effect', 'easing', 'delay', 'pause', 'duration'];
@@ -93,13 +89,36 @@ class MainCommand
         $query = html_entity_decode($query, ENT_QUOTES | ENT_HTML5, 'UTF-8');
         parse_str($query, $opts);
 
-        $res = array();
+        $res = [];
         foreach ($validOpts as $key) {
             $res[$key] = isset($opts[$key]) && is_string($opts[$key]) && $opts[$key] !== ""
                 ? $opts[$key]
                 : $this->conf["default_$key"];
         }
+        return $res;
+    }
 
+    /**
+     * @param list<Image> $images
+     * @return list<object{filename:string,name:string,webp:string,avif:string,style:string,loading:string}>
+     */
+    private function imageData(array $images): array
+    {
+        $res = [];
+        $first = true;
+        foreach ($images as $image) {
+            $res[] = (object) [
+                "filename" => $image->getFilename(),
+                "name" => $image->getName(),
+                "webp" => $image->hasWebp() ? $image->getWebp() : "",
+                "avif" => $image->hasAvif() ? $image->getAvif() : "",
+                "style" => $first
+                    ? "position: static; display: block; z-index: 1; width: 100%"
+                    : "position: absolute; display: none; width: 100%",
+                "loading" => $first ? "eager" : "lazy",
+            ];
+            $first = false;
+        }
         return $res;
     }
 }
--- views/slideshow.php
+++ views/slideshow.php
@@ -1,34 +1,35 @@
 <?php
 
 use Plib\View;
-use Slideshow\Model\Image;
 
 if (!isset($this)) {http_response_code(403); exit;}
 
 /**
  * @var View $this
  * @var string $script
- * @var array<string,string> $opts
- * @var list<Image> $imgs
- * @var list<string> $styles
- * @var list<string> $loading
+ * @var list<object{filename:string,name:string,webp:string,avif:string,style:string,loading:string}> $images
+ * @var string $effect
+ * @var string $easing
+ * @var string $delay
+ * @var string $pause
+ * @var string $duration
  */
 ?>
 
 <!-- Slideshow_XH slideshow -->
 <script type="module" src="<?=$this->esc($script)?>"></script>
 <div class="slideshow" style="position: relative; width: 100%; overflow: hidden"
-     data-effect="<?=$this->esc($opts['effect'])?>" data-easing="<?=$this->esc($opts['easing'])?>"
-     data-delay="<?=$this->esc($opts['delay'])?>" data-pause="<?=$this->esc($opts['pause'])?>" data-duration="<?=$this->esc($opts['duration'])?>">
-<?foreach ($imgs as $i => $img):?>
-  <picture style="<?=$this->esc($styles[$i])?>">
-<?  if ($img->hasAvif()):?>
-    <source srcset="<?=$this->esc($img->getAvif())?>" type="image/avif">
+     data-effect="<?=$this->esc($effect)?>" data-easing="<?=$this->esc($easing)?>"
+     data-delay="<?=$this->esc($delay)?>" data-pause="<?=$this->esc($pause)?>" data-duration="<?=$this->esc($duration)?>">
+<?foreach ($images as $image):?>
+  <picture style="<?=$this->esc($image->style)?>">
+<?  if ($image->avif):?>
+    <source srcset="<?=$this->esc($image->avif)?>" type="image/avif">
 <?  endif?>
-<?  if ($img->hasWebp()):?>
-    <source srcset="<?=$this->esc($img->getWebp())?>" type="image/webp">
+<?  if ($image->webp):?>
+    <source srcset="<?=$this->esc($image->webp)?>" type="image/webp">
 <?  endif?>
-    <img src="<?=$this->esc($img->getFilename())?>" alt="<?=$this->esc($img->getName())?>" loading="<?=$this->esc($loading[$i])?>">
+    <img src="<?=$this->esc($image->filename)?>" alt="<?=$this->esc($image->name)?>" loading="<?=$this->esc($image->loading)?>">
   </picture>
 <?endforeach?>
 </div>
