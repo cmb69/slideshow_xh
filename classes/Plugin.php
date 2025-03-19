0126f94da2479fdce35cee5f306cb8d6a7d1285a
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-19 16:07:37 +0100

    Stop the registerUserFunctions() madness

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -35,41 +35,11 @@ class Plugin
     {
         global $admin, $o;
 
-        self::registerUserFunctions();
         if (XH_ADM) { // @phpstan-ignore-line
             XH_registerStandardPluginMenuItems(false);
             if (XH_wantsPluginAdministration('slideshow')) {
                 $o .= print_plugin_admin('off')
                     . self::admin($admin);
-            }
-        }
-    }
-
-    /**
-     * @return void
-     */
-    private static function registerUserFunctions()
-    {
-        $rc = new ReflectionClass("\\Slideshow\\Plugin");
-        foreach ($rc->getMethods(ReflectionMethod::IS_PUBLIC) as $rm) {
-            if (substr_compare($rm->getName(), "Command", -strlen("Command")) === 0) {
-                $name = $rm->getName();
-                $lcname = substr(strtolower($name), 0, -strlen("Command"));
-                $params = $args = [];
-                foreach ($rm->getParameters() as $rp) {
-                    $param = $arg = "\${$rp->getName()}";
-                    if ($rp->isOptional()) {
-                        $default = var_export($rp->getDefaultValue(), true);
-                        $param .= " = " . $default;
-                    }
-                    $params[] = $param;
-                    $args[] = $arg;
-                }
-                $parameters = implode(", ", $params);
-                $arguments = implode(", ", $args);
-                $body = "return \\Slideshow\\Plugin::$name($arguments);";
-                $code = "function $lcname($parameters) {\n\t$body\n}";
-                eval($code);
             }
         }
     }
@@ -98,21 +68,6 @@ class Plugin
         $title = 'Slideshow ' . self::VERSION;
         ob_start();
         (new InfoCommand(new View(), new SystemChecker()))();
-        return ob_get_clean();
-    }
-
-    /**
-     * Returns the slideshow.
-     *
-     * @param string $path    The path of the image folder.
-     * @param string $options The options in form of a query string.
-     *
-     * @return string (X)HTML.
-     */
-    public static function slideshowCommand($path, $options = '')
-    {
-        ob_start();
-        (new MainCommand(new ImageRepo(), new View()))($path, $options);
         return ob_get_clean();
     }
 }
--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+use Slideshow\ImageRepo;
+use Slideshow\MainCommand;
 use Slideshow\Plugin;
+use Slideshow\View;
+
+/**
+ * Returns the slideshow.
+ *
+ * @param string $path    The path of the image folder.
+ * @param string $options The options in form of a query string.
+ *
+ * @return string (X)HTML.
+ */
+function slideshow($path, $options = '')
+{
+    ob_start();
+    (new MainCommand(new ImageRepo(), new View()))($path, $options);
+    return ob_get_clean();
+}
 
 Plugin::run();
