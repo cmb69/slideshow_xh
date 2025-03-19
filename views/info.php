45e47094901167df10b5a4a6456786b25a9a0c9f
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-19 19:00:35 +0100

    Update heading levels on info screen wrt CMSimple_XH ≥ 1.7.0

--- views/info.php
+++ views/info.php
@@ -13,30 +13,30 @@ if (!isset($this)) {http_response_code(403); exit;}
 
 <!-- Slideshow_XH info -->
 <h1>Slideshow <?=$this->esc($version)?></h1>
-<h4><?=$this->text('synopsis')?></h4>
+<h2><?=$this->text('synopsis')?></h2>
 <pre>&lt;?=slideshow('<?=$this->text('synopsis_folder')?>', '<?=$this->text('synopsis_options')?>')?&gt;
 {{{slideshow('<?=$this->text('synopsis_folder')?>', '<?=$this->text('synopsis_options')?>')}}}</pre>
 <dl>
-    <dt><?=$this->text('synopsis_folder')?>:</dt>
-    <dd><?=$this->text('synopsis_folder_desc')?></dd>
-    <dt><?=$this->text('synopsis_options')?>:</dt>
-    <dd><?=$this->text('synopsis_options_desc')?>
-    <dl>
-        <dt>order</dt>
-        <dd><?=$this->text('cf_default_order')?></dd>
-        <dt>effect</dt>
-        <dd><?=$this->text('cf_default_effect')?></dd>
-        <dt>easing</dt>
-        <dd><?=$this->text('cf_default_easing')?></dd>
-        <dt>delay</dt>
-        <dd><?=$this->text('cf_default_delay')?></dd>
-        <dt>pause</dt>
-        <dd><?=$this->text('cf_default_pause')?></dd>
-        <dt>duration</dt>
-        <dd><?=$this->text('cf_default_duration')?></dd>
-    </dl></dd>
+  <dt><?=$this->text('synopsis_folder')?>:</dt>
+  <dd><?=$this->text('synopsis_folder_desc')?></dd>
+  <dt><?=$this->text('synopsis_options')?>:</dt>
+  <dd><?=$this->text('synopsis_options_desc')?>
+  <dl>
+    <dt>order</dt>
+    <dd><?=$this->text('cf_default_order')?></dd>
+    <dt>effect</dt>
+    <dd><?=$this->text('cf_default_effect')?></dd>
+    <dt>easing</dt>
+    <dd><?=$this->text('cf_default_easing')?></dd>
+    <dt>delay</dt>
+    <dd><?=$this->text('cf_default_delay')?></dd>
+    <dt>pause</dt>
+    <dd><?=$this->text('cf_default_pause')?></dd>
+    <dt>duration</dt>
+    <dd><?=$this->text('cf_default_duration')?></dd>
+  </dl></dd>
 </dl>
-<h4><?=$this->text('syscheck')?></h4>
+<h2><?=$this->text('syscheck')?></h2>
 <?php foreach ($checks as $check):?>
 <p class="<?=$this->esc($check['class'])?>"><?=$this->esc($check['message'])?></p>
 <?php endforeach?>
--- views/slideshow.php
+++ views/slideshow.php
@@ -18,14 +18,14 @@ if (!isset($this)) {http_response_code(403); exit;}
 <!-- Slideshow_XH slideshow -->
 <script type="module" src="<?=$this->esc($script)?>"></script>
 <div class="slideshow" style="position: relative; width: 100%; overflow: hidden"
-        data-effect="<?=$this->esc($opts['effect'])?>" data-easing="<?=$this->esc($opts['easing'])?>"
-        data-delay="<?=$this->esc($opts['delay'])?>" data-pause="<?=$this->esc($opts['pause'])?>" data-duration="<?=$this->esc($opts['duration'])?>">
+     data-effect="<?=$this->esc($opts['effect'])?>" data-easing="<?=$this->esc($opts['easing'])?>"
+     data-delay="<?=$this->esc($opts['delay'])?>" data-pause="<?=$this->esc($opts['pause'])?>" data-duration="<?=$this->esc($opts['duration'])?>">
 <?php foreach ($imgs as $i => $img):?>
-    <picture style="<?=$this->esc($styles[$i])?>">
+  <picture style="<?=$this->esc($styles[$i])?>">
 <?php   if ($img->hasWebp()):?>
-        <source srcset="<?=$this->esc($img->getWebp())?>" type="image/webp">
+    <source srcset="<?=$this->esc($img->getWebp())?>" type="image/webp">
 <?php   endif?>
-        <img src="<?=$this->esc($img->getFilename())?>" alt="<?=$this->esc($img->getName())?>" loading="<?=$this->esc($loading[$i])?>">
-    </picture>
+    <img src="<?=$this->esc($img->getFilename())?>" alt="<?=$this->esc($img->getName())?>" loading="<?=$this->esc($loading[$i])?>">
+  </picture>
 <?php endforeach?>
 </div>
